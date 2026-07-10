1a63ee2802c493052b8e5653b7a25ce053add997
varak <varaksinaalla@mail.ru>
2026-07-10 23:50:42 +0300

    refactoring models for tasks app, create interface for tasks app

--- applications/tasks/models.py
+++ applications/tasks/models.py
@@ -1,22 +1,53 @@
 from django.db import models
+from django.conf import settings
 
 
 class Board(models.Model):
     name = models.CharField(max_length=55)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="boards",
+    )
+
+    def __str__(self):
+        return self.name
+
 
 class Column(models.Model):
-    board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name="columns")
+    board = models.ForeignKey(
+        Board, 
+        on_delete=models.CASCADE, 
+        related_name="columns"
+    )
     name = models.CharField(max_length=25)
     order = models.PositiveSmallIntegerField(default=0)
 
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ["order"]
+
+
 class Task(models.Model):
-    column = models.ForeignKey(Column, on_delete=models.CASCADE, related_name="tasks")
+    column = models.ForeignKey(
+        Column, 
+        on_delete=models.CASCADE, 
+        related_name="tasks"
+    )
     title = models.CharField(max_length=150)
     description = models.TextField(blank=True)
     order = models.PositiveIntegerField(default=0)
-    color = models.CharField(max_length=7)
+    color = models.CharField(max_length=7, default="#FFFFFF")
     is_archived = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     deadline = models.DateTimeField(null=True, blank=True)
     completed_at = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ["order"]
--- applications/tasks/views.py
+++ applications/tasks/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
+from .models import Board, Task, Column
 
 @login_required
 def index(request):
+    tasks = (
+            Board.objects
+            .filter(user=request.user)
+            .prefetch_related("columns__tasks")
+        )
     return render(
         request,
         'tasks/index.html',
-        {'section': 'tasks'}
+        {'section': 'tasks', 'data': tasks}
+    )
+
+
+@login_required
+def create_board(request):
+    if request.method == 'POST':
+        ...
+    return render(
+        request,
+        'tasks/create_board.html',
+        {}
     )
