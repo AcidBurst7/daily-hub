1337ebd1f36349aee72f4de077cdc796caefc161
varak <varaksinaalla@mail.ru>
2026-08-07 17:59:29 +0300

    update setting for prod

--- config/settings.py
+++ config/settings.py
@@ -179,3 +179,13 @@ REDIS_DB = env.int("REDIS_DB", default=1)
 #         "schedule": 60.0
 #     }
 # }
+
+CELERY_BROKER_URL = env(
+    "CELERY_BROKER_URL",
+    default="redis://redis:6379/0"
+)
+
+CELERY_RESULT_BACKEND = env(
+    "CELERY_RESULT_BACKEND",
+    default="redis://redis:6379/0"
+)
